967b2e92ee5831a58da899685d9a49b85f0f4ed6
Puthyrath Hy <puthyrathy@gmail.com>
2025-06-03 16:24:36 -0700

    Do 13.3

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const { Sequelize, QueryTypes } = require('sequelize')
+
+const sequelize = new Sequelize(process.env.DATABASE_URL)
+
+const main = async () => {
+  try {
+    // await sequelize.authenticate()
+    // console.log('Connection has been established successfully.')
+     // console.log(blogs)
+
+    const blogs = await sequelize.query('SELECT * FROM blogs', { type: QueryTypes.SELECT })
+    blogs.forEach(({ author, title, likes }) => {
+      console.log(`${author}: '${title}', ${likes} likes`)
+    })
+    sequelize.close()
+  } catch (error) {
+    console.error('Unable to connect to the database', error)
+  }
+}
+
+main()
