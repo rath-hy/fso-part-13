5df09abbeaf00eda4b2bfdc50ab8bfec9a45c667
Puthyrath Hy <puthyrathy@gmail.com>
2025-06-28 13:07:29 -0700

    Do 13.6 better

--- controllers/authors.js
+++ controllers/authors.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
+const { sequelize } = require('../util/db')
 
 const { User, Blog } = require('../models')
 
+//primitive solution
 router.get('/', async (req, res) => {
   const users = await User.findAll({
     include: {
@@ -17,6 +19,20 @@ router.get('/', async (req, res) => {
   )
 
   res.json(authorInformation)
+})
+
+//more advanced solution
+router.get('/', async (req, res) => {
+  const blogs = await Blog.findAll({
+    include: {
+      model: User,
+      attributes: []
+    },
+
+    attributes: [ [sequelize.col('user.name'), 'name'], [sequelize.fn('SUM', sequelize.col('likes')), 'total_likes'] ],
+    group: 'user.name'
+  })
+  res.json(blogs)
 })
 
 module.exports = router
